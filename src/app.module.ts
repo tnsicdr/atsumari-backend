f2c8c5b75fbcc34f33930b22e9d7c015748a9313
tensei <908739+tnsicdr@users.noreply.github.com>
2023-12-29 13:40:32 -0500

    chore: Use port variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: string;
+
+  constructor(configService: ConfigService) {
+    AppModule.port = configService.get('HTTP_PORT');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as chalk from 'chalk';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(AppModule.port || 6000);
+  console.log(
+    `${chalk.yellow('[Bootstrap]')} ${chalk.green(
+      `Application bootstrapped on Port ${AppModule.port || 6000}`,
+    )}`,
+  );
 }
 bootstrap();
